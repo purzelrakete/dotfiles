a0519cb7571d827c1a371fd56a9959ef7c04f52f
Rany Keddo <rany@soundcloud.com>
2013-01-31 10:30:56 +0100

    Added email sigs
    Depending on $FOCUS.

---
+++ .mutt/sig.s
@@ -0,0 +1,16 @@
+Rany Keddo
+Tech Lead, SoundCloud
+
+Mail & GTalk: what you think
+Skype: rany.keddo
+http://soundcloud.com/rany
+Rosenthaler Strasse 13, 101 19, Berlin, DE
+
+We're hiring. Check out http://soundcloud.com/jobs
+
+Limited registered at Company House, Cardiff, UK.
+Registered Office: London, UK.
+Company number 6343600
+Authorized Director: Alexander Ljung
+Local Branch Office Germany: AG Charlottenburg, HRB 110657B
+
