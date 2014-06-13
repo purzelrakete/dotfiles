652e86c09ea9bdd7607938f1b45c70edc842f312
Rany Keddo <rany@soundcloud.com>
2014-06-13 18:11:09 +0200

    Fixes common script loading, new email sigs

--- .mutt/sig.s
+++ .mutt/sig.s
@@ -1,15 +1,16 @@
 Rany Keddo
 Engineer
 
-Mail & GTalk: what you think
-Skype: rany.keddo
 http://soundcloud.com/rany
-Greifswalder Strasse 212, 10405 Berlin
+http://twitter.com/purzelrakete
 
-We're hiring. Check out http://soundcloud.com/jobs
+SoundCloud Ltd.
+Rheinsberger Str. 76/77
+10115 Berlin, Germany
++49 (0)172 2345679
 
-Limited registered at Company House, Cardiff, UK.
-Registered Office: London, UK.
-Company number 6343600
-Authorized Director: Alexander Ljung
-Local Branch Office Germany: AG Charlottenburg, HRB 110657B
+Managing Director: Alexander Ljung
+Incorporated in England & Wales with Company No. 6343600
+Local Branch Office
+AG Charlottenburg
+HRB 110657B
